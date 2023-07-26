b38f343ef0cdd5cd6375e999985d82fc744206bb
Gabriel Bettencourt <gabriel.vieira@tradesis.com.br>
2023-07-26 16:13:21 -0300

    testes

--- database/filacredito.php
+++ database/filacredito.php
@@ -14,12 +14,14 @@ if (isset($_GET['operacao'])) {
     $operacao = $_GET['operacao'];
 
     if ($operacao == "buscar") {
-        $submissoes = chamaAPI('http://10.145.0.233', '/bsweb/erp/neurotech/neuproposta.php?SAIDA=JSON&POR=VENDEDOR&FILIAL=' . $_POST['codigoFilial'] , null, 'GET');
+        $filial = $_POST['codigoFilial'];
+        $submissoes = chamaAPI('http://10.145.0.233', "/bsweb/erp/neurotech/neuproposta.php?SAIDA=JSON&POR=VENDEDOR&FILIAL=$filial", null, 'GET');
 
         if (isset($submissoes["rows"])) {
             $submissoes = $submissoes["rows"]; // TRATAMENTO DO RETORNO
         }
-
+        echo json_encode($submissoes);
+        echo json_encode(chamaAPI('http://10.145.0.233', "/bsweb/erp/neurotech/neuproposta.php?SAIDA=JSON&POR=VENDEDOR&FILIAL=$filial", null, 'GET'));
         echo json_encode($submissoes);
         return $submissoes;
     }

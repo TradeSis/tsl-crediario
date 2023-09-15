b68520a565a89281b60726a0a51e82204fea7d5c
Helio Tradesis <helio@tradesis.com.br>
2023-09-15 18:11:24 -0300

    teste IP

--- consultas/filacredito.php
+++ consultas/filacredito.php
@@ -4,6 +4,7 @@ include_once '../head.php';
 include_once '../database/filacredito.php';
 
 $IP = $_SERVER['REMOTE_ADDR'];
+echo $IP;
 
 $vfilial = explode(".", $IP);
 if ($vfilial[0] == 172 || $vfilial[0] == 192) {
